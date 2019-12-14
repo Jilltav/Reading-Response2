2ce75130d8dc6eb83216d6e0a8f6597b09890d48
Jilltav <55204812+Jilltav@users.noreply.github.com>
2019-12-14 01:50:35 -0500

    Card map 3

---
+++ map-test3/map-test3/js/main.js
@@ -0,0 +1,25 @@
+   //map option//
+
+   function initMap(){
+    var options = {
+        zoom: 17,
+        center: {lat: 43.8430214, lng: -79.5416512}
+
+
+
+    }
+
+    //new map//
+   var map = new
+       google.maps.Map(document.getElementById('map'), options);
+
+       //marker
+
+       var marker = new google.map.Marker({
+        position:{lat: 43.8430214, lng: -79.5416512},
+        map: map,
+       })
+   } 
+
+
+   
